fff1ab6b0a52f265709446c98fbaf324e6f13f30
ankitakk457 <100507794+ankitakk457@users.noreply.github.com>
2023-05-27 20:03:15 +0530

    Update power.cpp

--- Recursion/power.cpp
+++ Recursion/power.cpp
@@ -1,1 +1,19 @@
+#include<iostream>
+using namespace std;
 
+int power(int a,int b){
+    //base case
+    if(b==0){
+        return 1;
+    }
+    //recursion case
+    return power(a,b-1)*a;
+}
+
+int main(){
+    int a,b;
+    cin>>a>>b;
+    
+    int ans=power(a,b);
+    cout<<ans<<endl;
+}
